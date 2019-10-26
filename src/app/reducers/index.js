c838b71f6a681aa88eb3b25b7fbede33db48c38e
Karolis Lesevicius <karolis.lesevicius@gmail.com>
2019-10-26 10:55:23 +0300

    Extracted reducers

---
+++ src/app/reducers/index.js
@@ -0,0 +1,65 @@
+import {
+  ADD_TO_CART,
+  ADD_TO_FAVOURITES,
+  GET_PRODUCTS_START,
+  GOT_PRODUCTS,
+  GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_END
+} from '../actions';
+
+const INITIAL_PRODUCTS_STATE = {
+  products: [],
+  loading: false,
+  error: undefined
+};
+
+function productExists(products, product) {
+  return products.find(p => p.id === product.id);
+}
+
+function cartProducts(state = [], action) {
+  switch(action.type) {
+    case ADD_TO_CART:
+      if (!productExists(state, action.product)) {
+        return [...state, action.product];
+      } else {
+        return state;
+      }
+    default:
+      return state;
+  }
+}
+
+function favouriteProducts(state = [], action) {
+  switch(action.type) {
+    case ADD_TO_FAVOURITES:
+      if (!productExists(state, action.product)) {
+        return [...state, action.product];
+      } else {
+        return state;
+      }
+    default:
+      return state;
+  }
+}
+
+function products(state = INITIAL_PRODUCTS_STATE, action) {
+  switch(action.type) {
+    case GET_PRODUCTS_START:
+      return { ...state, loading: true, error: undefined };
+    case GET_PRODUCTS_END:
+        return { ...state, loading: false };
+    case GET_PRODUCTS_ERROR:
+        return { ...state, error: 'Oh no! Something went wrong!' };
+    case GOT_PRODUCTS:
+      return {...state, products: action.products };
+    default:
+      return state;
+  }
+}
+
+export default {
+  products,
+  cartProducts,
+  favouriteProducts
+};
--- src/index.js
+++ src/index.js
@@ -5,71 +5,9 @@ import './index.scss';
 import App from './app';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import {
-  ADD_TO_CART,
-  ADD_TO_FAVOURITES,
-  GET_PRODUCTS_START,
-  GOT_PRODUCTS,
-  GET_PRODUCTS_ERROR,
-  GET_PRODUCTS_END
-} from './app/actions';
+import reducers from './app/reducers';
 
-const INITIAL_PRODUCTS_STATE = {
-  products: [],
-  loading: false,
-  error: undefined
-};
-
-function productExists(products, product) {
-  return products.find(p => p.id === product.id);
-}
-
-function cartProducts(state = [], action) {
-  switch(action.type) {
-    case ADD_TO_CART:
-      if (!productExists(state, action.product)) {
-        return [...state, action.product];
-      } else {
-        return state;
-      }
-    default:
-      return state;
-  }
-}
-
-function favouriteProducts(state = [], action) {
-  switch(action.type) {
-    case ADD_TO_FAVOURITES:
-      if (!productExists(state, action.product)) {
-        return [...state, action.product];
-      } else {
-        return state;
-      }
-    default:
-      return state;
-  }
-}
-
-function products(state = INITIAL_PRODUCTS_STATE, action) {
-  switch(action.type) {
-    case GET_PRODUCTS_START:
-      return { ...state, loading: true, error: undefined };
-    case GET_PRODUCTS_END:
-        return { ...state, loading: false };
-    case GET_PRODUCTS_ERROR:
-        return { ...state, error: 'Oh no! Something went wrong!' };
-    case GOT_PRODUCTS:
-      return {...state, products: action.products };
-    default:
-      return state;
-  }
-}
-
-const fakeEShopReducer = combineReducers({
-  cartProducts,
-  favouriteProducts,
-  products
-})
+const fakeEShopReducer = combineReducers(reducers);
 
 const store = createStore(
   fakeEShopReducer,
