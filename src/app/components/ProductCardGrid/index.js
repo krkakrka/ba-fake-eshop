bc4a60bef5b0698eadaadaab957aee546edbdede
Karolis Lesevicius <karolis.lesevicius@gmail.com>
2019-10-26 11:31:10 +0300

    Removed useless index.js

--- src/app/components/ProductCardGrid/index.js
+++ src/app/components/ProductCardGrid/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.scss';
-import { ProductCard } from '..';
+import ProductCard from '../ProductCard';
 
 function ProductCardGrid({ products }) {
   return (
